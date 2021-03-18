3532a70c7dd4a28245cbdb5b6341264bfa121bcd
Sujin Lee <bbonggi8@snu.ac.kr>
2021-03-18 10:38:12 +0900

    saved with encoding

--- attendance.R
+++ attendance.R
@@ -19,7 +19,7 @@ p_load("tidyverse", "readxl", "magrittr", "stringr", "data.table", "lubridate", 
 
 #### List of all students in the class ####
 
-list <- read_excel("G:/°øÀ¯ µå¶óÀÌºê/±İÀ¶°æÁ¦¼¼¹Ì³ª/participants_list.xlsx")
+list <- read_excel("G:/ê³µìœ  ë“œë¼ì´ë¸Œ/ê¸ˆìœµê²½ì œì„¸ë¯¸ë‚˜/participants_list.xlsx")
 # data from class
 
 
@@ -27,13 +27,13 @@ list <- list[-1]
 colnames(list) <- c("major", "id", "name", "type", "email", "hp")
 
 students <- list %>%
-  filter(type == "ÇĞ»ı") %>%
+  filter(type == "í•™ìƒ") %>%
   dplyr::select(major, id, name)
 # filter only students (exclude TA, professor)
 
 
 #### 3/17 ####
-march17 <- fread("G:/°øÀ¯ µå¶óÀÌºê/±İÀ¶°æÁ¦¼¼¹Ì³ª/participants_0317.csv")
+march17 <- fread("G:/ê³µìœ  ë“œë¼ì´ë¸Œ/ê¸ˆìœµê²½ì œì„¸ë¯¸ë‚˜/participants_0317.csv")
 # zoom records
 
 temp <- march17
@@ -43,25 +43,25 @@ temp %<>% dplyr::select(-email, -geust)
 
 temp %<>%
   mutate(
-    nick2 = str_replace(nick, "([°¡-ÆR][°¡-ÆR]) ([°¡-ÆR])", "\\2\\1"),
-    # ¼öÁø ÀÌ --> ÀÌ¼öÁø
-    nick3 = str_replace(nick2, "([°¡-ÆR]) ([°¡-ÆR][°¡-ÆR])", "\\1\\2"),
-    # ÀÌ ¼öÁø --> ÀÌ¼öÁø
-    nick4 = str_replace(nick3, "([°¡-ÆR]) ([°¡-ÆR])", "\\2\\1"), 
-    # Âù Çã --> ÇãÂù
-    name1 = str_extract(nick4, "[°¡-ÆR][°¡-ÆR][°¡-ÆR]"),
-    # ÇĞ¹ø, ÀÌ¸§ ´Ù ÀÖ´Â ¿­¿¡¼­ ¼¼ ±ÛÀÚ ÀÌ¸§ ÃßÃâ
-    name2 = str_extract(nick4, "[°¡-ÆR][°¡-ÆR]"),
-    # ÇĞ¹ø, ÀÌ¸§ ´Ù ÀÖ´Â ¿­¿¡¼­ µÎ ±ÛÀÚ ÀÌ¸§ ÃßÃâ
+    nick2 = str_replace(nick, "([ê°€-í£][ê°€-í£]) ([ê°€-í£])", "\\2\\1"),
+    # ìˆ˜ì§„ ì´ --> ì´ìˆ˜ì§„
+    nick3 = str_replace(nick2, "([ê°€-í£]) ([ê°€-í£][ê°€-í£])", "\\1\\2"),
+    # ì´ ìˆ˜ì§„ --> ì´ìˆ˜ì§„
+    nick4 = str_replace(nick3, "([ê°€-í£]) ([ê°€-í£])", "\\2\\1"), 
+    # ì°¬ í—ˆ --> í—ˆì°¬
+    name1 = str_extract(nick4, "[ê°€-í£][ê°€-í£][ê°€-í£]"),
+    # í•™ë²ˆ, ì´ë¦„ ë‹¤ ìˆëŠ” ì—´ì—ì„œ ì„¸ ê¸€ì ì´ë¦„ ì¶”ì¶œ
+    name2 = str_extract(nick4, "[ê°€-í£][ê°€-í£]"),
+    # í•™ë²ˆ, ì´ë¦„ ë‹¤ ìˆëŠ” ì—´ì—ì„œ ë‘ ê¸€ì ì´ë¦„ ì¶”ì¶œ
     name = ifelse(is.na(name1), name2, name1)
-    # ÀÌ¸§ÀÌ ¼¼ ±ÛÀÚ°¡ ¾Æ´Ï¸é µÎ ±ÛÀÚ ÀÌ¸§À¸·Î.
+    # ì´ë¦„ì´ ì„¸ ê¸€ìê°€ ì•„ë‹ˆë©´ ë‘ ê¸€ì ì´ë¦„ìœ¼ë¡œ.
     ) 
 
 temp %>% filter(is.na(name)) %>% dplyr::select(nick)
-# ÇÑ±Û ÀÌ¸§ ¾ø´Â »ç¶÷ È®ÀÎ
+# í•œê¸€ ì´ë¦„ ì—†ëŠ” ì‚¬ëŒ í™•ì¸
 
-temp[is.na(temp$name), c("name")] = c("È«±æµ¿", "ÀÌ¿µÈñ", "±è¿µ¼÷")
-# ¼ø¼­´ë·Î ½áÁÖ±â
+temp[is.na(temp$name), c("name")] = c("í™ê¸¸ë™", "ì´ì˜í¬", "ê¹€ì˜ìˆ™")
+# ìˆœì„œëŒ€ë¡œ ì¨ì£¼ê¸°
 
 temp %<>% 
   arrange(name) %>%
@@ -75,7 +75,7 @@ temp %<>%
 # Keep only the variables in need and convert attend and exit variables to time format
 
 temp %<>% filter(exit >= "2021-03-17 00:59:59")
-# ¼ö¾÷ 1½Ã Àü¿¡ ³ª°£ ±â·ÏÀº Á¦¿Ü
+# ìˆ˜ì—… 1ì‹œ ì „ì— ë‚˜ê°„ ê¸°ë¡ì€ ì œì™¸
 
 temp %<>% 
   group_by(name) %>%
@@ -85,23 +85,23 @@ temp %<>%
     duration = sum(duration, na.rm = TRUE)
     ) %>%
   ungroup
-# ÇĞ»ıº°·Î °¡Àå ÀÌ¸¥ Á¢¼Ó½Ã°£, °¡Àå ³ªÁßÀÇ ÅğÀå ½Ã°£°ú ÃÑ Á¢¼Ó½Ã°£
+# í•™ìƒë³„ë¡œ ê°€ì¥ ì´ë¥¸ ì ‘ì†ì‹œê°„, ê°€ì¥ ë‚˜ì¤‘ì˜ í‡´ì¥ ì‹œê°„ê³¼ ì´ ì ‘ì†ì‹œê°„
 
 temp %<>% 
   mutate(
     late = ifelse(attend > "2021-03-17 01:30:00", 1, 0),
-    # Ã¹ ÀÔÀåÀÌ 1½Ã 30ºĞ ÀÌÈÄ¸é Áö°¢ Ã³¸®
+    # ì²« ì…ì¥ì´ 1ì‹œ 30ë¶„ ì´í›„ë©´ ì§€ê° ì²˜ë¦¬
     pass = ifelse(duration >= 80, 1, 0)
-    # ÃÑ 80ºĞ ÀÌ»ó ¼ö°­ÇßÀ¸¸é Ãâ¼® Ã³¸®
+    # ì´ 80ë¶„ ì´ìƒ ìˆ˜ê°•í–ˆìœ¼ë©´ ì¶œì„ ì²˜ë¦¬
     )
 
 colnames(temp) <- paste(colnames(temp), "0317", sep = "_")
 
 students %<>% left_join(., temp, by = c("name" = "name_0317"))
-# Ãâ¼®ºÎ ¸ñ·Ï¿¡ ¿¬°á
+# ì¶œì„ë¶€ ëª©ë¡ì— ì—°ê²°
 
 students$pass_0317[is.na(students$pass_0317)] = 0
-# ÇØ´ç ¼ö¾÷ÀÏ¿¡ µ¥ÀÌÅÍ°¡ ¾øÀ¸¸é °á¼® Ã³¸®
+# í•´ë‹¹ ìˆ˜ì—…ì¼ì— ë°ì´í„°ê°€ ì—†ìœ¼ë©´ ê²°ì„ ì²˜ë¦¬
 
 students %<>% 
   dplyr::select(major, id, name, starts_with("pass"), starts_with("late"), everything())
